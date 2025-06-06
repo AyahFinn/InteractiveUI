3b5ca93183f99bffd99c57a43501a867563fba9f
Ayah <ayah29393@gmail.com>
2025-06-06 09:41:33 -0500

    Aded Button

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-       
+    @State private var name = ""
+    @State private var textTitle = " What is your name"
     
+    var body: some View {
+        VStack {
+            Text(" What is your name?")
+                .font(.title)
+            TextField(/*@START_MENU_TOKEN@*/"Type your name here...."/*@END_MENU_TOKEN@*/, text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray,width:1)
+            Button("Submit Name") {
+                textTitle = "Welcome, \(name)!🪸🤿🐠🌴"
+            }
+        //end buttton
+            .font(.title)
+            .buttonStyle(.borderedProminent)
+            .tint(Color("Spanish Orange"))
+            
+        }
+        //end Vstack
+        .padding()
     }
 //end body
 }
